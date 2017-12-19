32e42bb318747458a9ffdaf1f43ba8221ff367a6
zbing <openskypod@gmail.com>
2017-12-20 00:05:44 +0800

    first

---
+++ PHPRuntimes.php
@@ -0,0 +1,23 @@
+<?php
+
+class PHPRuntimes
+{
+	/**
+	 * Author zbing
+	 */
+
+	protected $marks = [];
+
+	function mark($mark=null)
+	{
+		return ['time'=>microtime(true), 'mark'=>$mark];
+	}
+
+	function display()
+	{
+		foreach($this->times as $key=>$value){
+			if ($key==0) continue;
+			echo "【= " . ($times[$key-1]['mark'] ?? 'Mark['.$key.']') .'-'. ($value['mark'] ?? 'Mark['.($key+1).']') .": ". ($value['time'] - $times[$key-1]['time']) . " =】";
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# php-runtimes
-Simple PHP Runtimes mark.
+# PHP-Runtimes
+Simple script Calculate the PHP runtime.
+计算PHP运行时间的小脚本
+
+----------
+
+
+
+
+有时（尤其在优化网站的时候）需要分析PHP脚本的运行时间，利用 `microtime()` 函数可以很方便的获取到，此类基于 `microtime()` 函数进行了一层封装。
+
+
+Demo:
+```python
+$runtime = new PHPRuntimes();
+
+$runtime->mark('mark1');
+    some code...
+$runtime->mark('mark2');
+    some code...
+    
+$runtime->display();
+```
+
+and
+
+```php >= 7.0```
